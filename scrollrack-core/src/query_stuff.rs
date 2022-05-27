7aec8c70fb95d8adeafbba902ee853f7c803dbc7
Daniel Gusenburger <danielgusenburger@online.de>
2022-05-27 23:02:43 +0200

    refactor renamed no-promos to include-promos flag

--- scrollrack-core/src/query_stuff.rs
+++ scrollrack-core/src/query_stuff.rs
@@ -5,46 +5,57 @@ use std::collections::HashMap;
 
 pub type CardsBySet = HashMap<SetInfo, Vec<card::Card>>;
 
-/// For a given card name, query the scryfall API to fetch all printings.
-pub fn query(info: CardInfo, no_promos: bool) -> Vec<(SetInfo, card::Card)> {
-    let card_query = if no_promos {
-        Query::And(vec![exact(info.name()), not(PrintingIs::Promo)])
-    } else {
-        Query::And(vec![exact(info.name())])
-    };
-    SearchOptions::new()
-        .unique(UniqueStrategy::Prints)
-        .query(card_query)
-        .search_all()
-        .map_or(vec![], |res| {
-            res.iter()
-                .map(|scryinfo| {
-                    (
-                        SetInfo::new(&scryinfo.set_name, scryinfo.set_uri.to_owned()),
-                        scryinfo.to_owned(),
-                    )
-                })
-                .collect()
-        })
+pub struct CardQuery {
+    include_promos: bool,
+    _inverted_mapping: bool,
 }
 
-pub fn merge_results(results: Vec<(SetInfo, card::Card)>) -> CardsBySet {
-    let mut merged = CardsBySet::new();
-    results.into_iter().for_each(|res| {
-        merged.entry(res.0).or_insert(Vec::new()).push(res.1);
-    });
-    merged
-        .values_mut()
-        .for_each(|cards| cards.dedup_by_key(|card| card.name.to_owned()));
-    merged
-}
+impl CardQuery {
+    fn single_query(&self, info: &CardInfo) -> Vec<(SetInfo, card::Card)> {
+        SearchOptions::new()
+            .unique(UniqueStrategy::Prints)
+            .query(self.assemble_query(info))
+            .search_all()
+            .map_or(vec![], |res| {
+                res.iter()
+                    .filter(|scryinfo| scryinfo.games.contains(&scryfall::card::Game::Paper))
+                    .map(|scryinfo| {
+                        (
+                            SetInfo::new(&scryinfo.set_name, scryinfo.set_uri.to_owned()),
+                            scryinfo.to_owned(),
+                        )
+                    })
+                    .collect()
+            })
+    }
 
-pub fn query_and_merge_all(cards: Vec<CardInfo>, no_promos: bool) -> CardsBySet {
-    merge_results(
-        cards
-            .into_iter()
-            .map(|c| query(c, no_promos))
-            .flatten()
-            .collect(),
-    )
+    fn assemble_query(&self, info: &CardInfo) -> Query {
+        let mut query_vec = vec![exact(info.name())];
+        if !self.include_promos {
+            query_vec.push(not(PrintingIs::Promo));
+        }
+
+        Query::And(query_vec)
+    }
+
+    fn merge_results(&self, results: impl Iterator<Item = (SetInfo, card::Card)>) -> CardsBySet {
+        let mut merged = CardsBySet::new();
+        results.into_iter().for_each(|res| {
+            merged.entry(res.0).or_insert(Vec::new()).push(res.1);
+        });
+        merged
+            .values_mut()
+            .for_each(|cards| cards.dedup_by_key(|card| card.name.to_owned()));
+        merged
+    }
+
+    pub fn with_options(include_promos: bool, _inverted_mapping: bool) -> Self {
+        CardQuery {
+            include_promos,
+            _inverted_mapping,
+        }
+    }
+    pub fn query(&self, cards: impl Iterator<Item = CardInfo>) -> CardsBySet {
+        self.merge_results(cards.map(|c| self.single_query(&c)).flatten())
+    }
 }
