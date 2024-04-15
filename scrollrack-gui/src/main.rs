1535320065b9b37d478707481fa32a8cb796eeac
niomate <15348231+niomate@users.noreply.github.com>
2024-04-15 17:48:28 +0200

    interim commit, updated dioxus version. not working

--- scrollrack-gui/src/main.rs
+++ scrollrack-gui/src/main.rs
@@ -5,13 +5,14 @@ use scrollrack_core::parse;
 use dioxus::prelude::*;
 
 fn main() {
-    dioxus_desktop::launch(app);
+    launch(app);
 }
 
-fn app(cx: Scope) -> Element {
+fn app() -> Element {
     let lines = parse::read_lines("docs/affinity.txt").unwrap();
 
-    let cards_by_set = CardQuery::default().run(parse::parse_card_infos(lines));
+    let cards_by_set =
+        use_future(move || async move { CardQuery::default().run(parse::parse_card_infos(lines)) });
 
     let cards_sorted = cards_by_set
         .keys()
@@ -19,13 +20,13 @@ fn app(cx: Scope) -> Element {
         .map(|k| (k, cards_by_set.get(k).unwrap()))
         .collect::<Vec<_>>();
 
-    cx.render(rsx!(
+    rsx! {
             ul {
                 cards_sorted.iter().map(|(set, cards)| {
                     rsx!( li { "{set.set_name()}" ul {
                         cards.iter().map(|card| rsx!( li { "{card.format_detailed()}" }))
                     } } )
-                })
+                }),
             }
-    ))
+    }
 }
