9cff67c02583e223daeafc272f6453f4204d7862
niomate <d.gusenburger@zema.de>
2024-09-17 14:26:09 +0200

    fixed CORS implementation

--- scrollrack-backend/src/main.rs
+++ scrollrack-backend/src/main.rs
@@ -1,7 +1,9 @@
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
 use rocket::http::Method;
 use rocket::http::Status;
 use rocket::serde::{json::Json, Deserialize};
-use rocket_cors::{AllowedOrigins, CorsOptions};
+use rocket::{Request, Response};
 use scrollrack_core::card_query::CardQuery;
 use scrollrack_core::cardinfo::CardInfo;
 use scrollrack_core::output::format::OutputFormat;
@@ -36,22 +38,40 @@ async fn decklist_table(cards: Json<Vec<&'_ str>>) -> String {
     render_to_string::<SortByName, OutputTable>(cards_by_set).unwrap_or("Error".to_owned())
 }
 
+/// Catches all OPTION requests in order to get the CORS related Fairing triggered.
+#[options("/<_..>")]
+fn all_options() {
+    /* Intentionally left empty */
+}
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
-    let cors = CorsOptions::default()
-        .allowed_origins(AllowedOrigins::all())
-        .allowed_methods(
-            vec![Method::Get, Method::Post]
-                .into_iter()
-                .map(From::from)
-                .collect(),
-        )
-        .allow_credentials(true);
-
     rocket::build()
         .mount(
             "/api/endpoints",
-            routes![single_card, decklist_table, decklist_raw,],
+            routes![single_card, decklist_table, decklist_raw, all_options],
         )
-        .attach(cors.to_cors().unwrap())
+        .attach(CORS)
 }
